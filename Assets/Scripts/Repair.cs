0ee0b29a2ae0bef8e40b8b51fe220138c9b18548
CarsonCuster <carsonwcuster@gmail.com>
2020-02-01 18:31:23 -0500

    Added Repair script

---
+++ Assets/Scripts/Repair.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Repair : MonoBehaviour
+{
+    public GameObject button;
+    public SpriteRenderer sr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = gameObject.AddComponent<SpriteRenderer>() as SpriteRenderer;
+    }
+
+    public void Fix()
+    {
+        button.SetActive(true);
+    }
+    
+}
