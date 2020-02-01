847d34d8f3eceddb7a769032d57e531a41e13975
Michael Douglas <michaelperezdouglas@gmail.com>
2020-02-01 17:22:41 -0500

    Added Random Text leaderboard

--- Assets/Scripts/Leaderboard.cs
+++ Assets/Scripts/Leaderboard.cs
@@ -1,16 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Leaderboard : MonoBehaviour
 {
+    public TextAsset randomNames;
     public string[] names = new string[10];
-
-    public void Start(){
- for(int i = 0;i<10;i++)
+    public Text leaderboardText;
+    [ContextMenu("Test some names")]
+    public void Start()
+    {
+        SetRandomNames();
+        UpdateTextWithNames();    
+    }
+    public void UpdateTextWithNames()
+    {
+        
+        string leaderboardString = "";
+        for(int i = 0;i<names.Length;i++)
         {
-            string randomArrayString = names[Random.Range(0, names.Length)];
-            Debug.Log(randomArrayString);
+            // string randomArrayString = names[Random.Range(0, names.Length)];
+            // Debug.Log(randomArrayString);
+            leaderboardString = leaderboardString + (i+1)+". "+ names[i]+ "\n";
         }
-}
+        leaderboardText.text = leaderboardString;
+    }
+    
+    void SetRandomNames()
+    {
+        string[] allNames = randomNames.text.Split('\n');
+        for(int i = 0;i<5;i++){
+            names[i] = allNames[Random.Range(0,allNames.Length)];
+        }
+    }
 }
