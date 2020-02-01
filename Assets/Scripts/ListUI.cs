3bd70b1258372bbd3d5c6031ff693a9f0e0b682c
CarsonCuster <carsonwcuster@gmail.com>
2020-02-01 15:23:44 -0500

    Added script folder and list script

---
+++ Assets/Scripts/ListUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ListUI : MonoBehaviour
+{
+    public GameObject menu;
+
+    void Start()
+    {
+        menu.SetActive(false);
+    }
+
+    public void Dropdown()
+    {
+        menu.SetActive(!menu.active);
+    }
+    
+}
