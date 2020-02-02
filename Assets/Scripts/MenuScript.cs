627f58ff3ee57b090eedbe09c97b4afd06c99692
atmusick <a0musick@gmail.com>
2020-02-01 21:05:54 -0500

    Added Intro Scene

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartGame()
+    {
+        Application.LoadLevel("SampleScene");
+    }
+
+    // Update is called once per frame
+   
+}
